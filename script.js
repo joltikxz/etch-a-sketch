3a07c4626670f364992154baed38748ad7e2a830
joltikxz <adrianpaulbarameda@gmail.com>
2025-05-04 15:27:22 +0800

    Addition of HTML, CSS, JS

---
+++ script.js
@@ -0,0 +1,63 @@
+const gridContainer = document.querySelector('.grid-container');
+const inputBtn = document.querySelector('.input');
+let opacity;
+
+function createGrid(size) {
+    opacity = 0.10;
+
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    } 
+
+    for (let i=0; i<size; i++) {
+        for (let j=0; j<size; j++) {
+    
+            let gridTile = document.createElement('div');
+            
+            let dimension = parseInt(960 / size);
+            gridTile.style.height = `${dimension}px`;
+            gridTile.style.width = `${dimension}px`;
+
+            //applying the styles
+            gridTile.setAttribute('class', 'tile');
+            gridContainer.appendChild(gridTile);
+    
+            //setting up the event listener
+            gridTile.addEventListener('mouseover', () => {
+                gridTile.style.backgroundColor = randomRGB();
+                gridTile.style.opacity = progressiveOpacity();
+            });
+        }
+    }
+}
+
+function randomRGB() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function progressiveOpacity() {
+    let temp = opacity;
+    if (temp === 1.0) {
+        return temp;
+    } else {
+        opacity += 0.10;
+        return temp;
+    }
+}
+
+createGrid(16);
+
+inputBtn.addEventListener('click', () => {
+    let size;
+    do {
+        size = +prompt('Enter the size of the grid: ');
+    } while (size > 100);
+
+    createGrid(size);
+});
+
+
